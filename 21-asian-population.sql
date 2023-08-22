b38218ce5222b46dc0be75d87640c6eb9d5669df
Haydar <39428898+haydarmiezanie@users.noreply.github.com>
2023-08-22 19:18:13 +0700

    Update 21-asian-population.sql
    check the grammar

--- 21-asian-population.sql
+++ 21-asian-population.sql
@@ -1,4 +1,4 @@
---this is query for sum population in asia continent
+--This is a query for the sum population in the Asia continent.
 
 select sum(ct.population) as rata_rata_populasi
 from city as ct inner join country as ct2
