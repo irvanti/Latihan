d47f9b887300d689aebecff1028b0010a7b48636
irvanti <ismailirvant@gmail.com>
2023-09-15 22:11:30 +0700

    sorry banget dar itu typo harusnya sintax set dan ; tetep kepake dibelakang 21

--- HackerRank/54-draw-the-triangle-2.sql
+++ HackerRank/54-draw-the-triangle-2.sql
@@ -1,6 +1,6 @@
 --this is query to draw the triangle as ascending stars
 
-et @temp:=0
+set @temp:=0;
 select repeat('* ', @temp:= @temp + 1) 
 from information_schema.tables
 where @temp < 20;
--- HackerRank/55-draw-the-triangle-1.sql
+++ HackerRank/55-draw-the-triangle-1.sql
@@ -1,5 +1,5 @@
 --this is query to draw the triangle as descending stars
 
-et @temp:=21
+set @temp:=21;
 select repeat('* ', @temp:= @temp - 1) 
 from information_schema.tables;
