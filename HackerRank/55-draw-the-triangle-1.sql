b2a3e231607111966eb3f9dc3e687b09082b1796
irvanti <ismailirvant@gmail.com>
2023-09-15 21:36:05 +0700

    sorry sintax ; buat error tado

--- HackerRank/55-draw-the-triangle-1.sql
+++ HackerRank/55-draw-the-triangle-1.sql
@@ -1,5 +1,5 @@
 --this is query to draw the triangle as descending stars
 
-et @temp:=21; 
+et @temp:=21
 select repeat('* ', @temp:= @temp - 1) 
-from information_schema.tables
+from information_schema.tables;
